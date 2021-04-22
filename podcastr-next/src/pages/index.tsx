42d8291381f2f7912bfa208485440e01aa2f1eea
evenilsonlp <evenilsonlp@gmail.com>
2021-04-22 03:40:03 -0300

    Fazendo a página de Epsódio

--- podcastr-next/src/pages/index.tsx
+++ podcastr-next/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from "next";
 import Image from 'next/image'
+import Link from 'next/link'
 import { format, parseISO } from "date-fns"
 import ptBR from 'date-fns/locale/pt-BR'
 import { api } from "../services/api";
@@ -38,8 +39,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <Image width={192} height={192} objectFit='cover' src={episode.thumbnail} alt={episode.title}/>
 
                   <div className={styles.episodeDetails}>
-                    <a href="">{episode.title}</a>
-
+                    <Link href={`/episodes/${episode.id}`} >
+                      <a >{episode.title}</a>
+                    </Link>
                     <p>{episode.members}</p>
                     <span>{episode.publishedAt}</span>
                     <span>{episode.durationAsString}</span>
@@ -59,12 +61,15 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
         <table cellSpacing={0}>
           <thead>
-            <th></th>
-            <th>Podcast</th>
-            <th>Integrantes</th>
-            <th>Data</th>
-            <th>Duração</th>
-            <th></th>
+            <tr>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </tr>
+              
           </thead>
           <tbody>
             {
@@ -75,7 +80,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                       <Image width={192} height={192} objectFit='cover' src={episode.thumbnail} alt={episode.title}/>
                     </td>
                     <td >
-                      <a href="">{episode.title}</a>
+                      <Link href={`/episodes/${episode.id}`} >
+                        <a >{episode.title}</a>
+                      </Link>
                     </td>
                     <td>{episode.members}</td>
                     <td style={{ width:100}}>{episode.publishedAt}</td>
