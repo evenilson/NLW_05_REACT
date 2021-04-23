9c7690821ff7cddf36b34fe3a7f412fd5f109228
evenilsonlp <evenilsonlp@gmail.com>
2021-04-23 00:22:56 -0300

    Usando refs no React

--- podcastr-next/src/components/Player/index.tsx
+++ podcastr-next/src/components/Player/index.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { PLayerContext } from '../../contexts/PlayerContext';
 import Slider from 'rc-slider';
 
@@ -10,9 +10,23 @@ import styles from './styles.module.scss';
 
 
 export function Player() {
-  const { episodeList, currentEpisodeIndex } = useContext(PLayerContext)
+  const { episodeList, currentEpisodeIndex, isPlaying, togglePlay } = useContext(PLayerContext)
+
+  const audioRef = useRef<HTMLAudioElement>(null);
 
   const episode = episodeList[currentEpisodeIndex]
+
+  useEffect(() => {
+    if (!audioRef.current) {
+      return;
+    }
+
+    if (isPlaying) {
+      audioRef.current.play();
+    }else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying])
 
   return (
     <div className={styles.playerContainer}>
@@ -58,7 +72,9 @@ export function Player() {
           { episode && (
             <audio 
               src={episode.url}
+              ref={audioRef}
               autoPlay
+              
             />
           )}
 
@@ -70,8 +86,12 @@ export function Player() {
           <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
-          <button type="button" className={styles.playButton} disabled={!episode}>
-            <img src="/play.svg" alt="Tocar" />
+          <button type="button" className={styles.playButton} disabled={!episode} onClick={togglePlay}>
+            { isPlaying ? (
+              <img src="/pause.svg" alt="Tocar" />
+            ) : (
+              <img src="/play.svg" alt="Pausar" />
+            )}
           </button>
           <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar próximo" />
--- podcastr-next/src/contexts/PlayerContext.ts
+++ podcastr-next/src/contexts/PlayerContext.ts
@@ -11,7 +11,9 @@ type Episode = {
 type PlayerContextData = {
     episodeList: Episode[];
     currentEpisodeIndex: number;
+    isPlaying: boolean;
     play: (episode: Episode) => void;
+    togglePlay: () =>void;
 }
 
 export const PLayerContext = createContext({} as PlayerContextData);
